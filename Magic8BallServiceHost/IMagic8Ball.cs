36abce76c01b46f1a31a1924876e24a6e287d3ba
Richard Turner <rich@bitcrazed.com>
2011-11-29 15:16:47 -0800

    Exposed service via 'webHttpBinding' endpoint to support JSON clients

--- Magic8BallServiceHost/IMagic8Ball.cs
+++ Magic8BallServiceHost/IMagic8Ball.cs
@@ -1,4 +1,5 @@
 ﻿using System.ServiceModel;
+using System.ServiceModel.Web;
 
 namespace Magic8BallServiceHost
 {
@@ -6,6 +7,7 @@ namespace Magic8BallServiceHost
     public interface IMagic8Ball
     {
         [OperationContract]
+        [WebGet(UriTemplate = "shake/{question}")]
         string Shake(string question);
     }
 }
